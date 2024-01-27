219482aed10777f8459b1a62238c3ab5d965fe71
waffleattack <tbaeyens411@gmail.com>
2024-01-27 11:12:27 -0500

    Yoinky Sploinky

--- src/main/java/frc/robot/commands/AlignToSpeaker.java
+++ src/main/java/frc/robot/commands/AlignToSpeaker.java
@@ -17,7 +17,7 @@ public class AlignToSpeaker extends Command {
 
         // TODO: Tune these
         public PIDController yPID = new PIDController(10.0, 1 ,0.60);
-        public PIDController omegaPID = new PIDController(10.0, 0, 0.60);
+        public PIDController omegaPID = new PIDController(0, 0, 0.);
 
         public double targetY = 5.5;
         public double targetRotation = Units.degreesToRadians((DriverStation.getAlliance().orElse(Alliance.Blue).equals(Alliance.Blue)) ? 0 : 180);
@@ -37,30 +37,35 @@ public class AlignToSpeaker extends Command {
 
                 //System.out.println(currentRotation);
                 //System.out.println(targetRotation);
-                //System.out.println(Math.abs(targetRotation - currentRotation));
+                //System.out.println(Math.abs(targetRotation - currentRotation))
+                //System.out.println(targetY - currentY);
 
-                System.out.println(targetY - currentY);
-                        double vY = yPID.calculate(currentY, targetY);
-                        if(vY > 1){
-                                vY = 1;
-                        }else if(vY < -1){
-                                vY = -1;
-                        }
+                double vY = yPID.calculate(currentY, targetY);
 
-                        
+                if(vY > 1){
+                         vY = 1;
+                }else if(vY < -1){
+                        vY = -1;
+                }
 
-                        drive.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
-                                        0,
-                                        vY,
-                                        omegaPID.calculate(currentRotation, targetRotation),
-                                        drive.getPose().getRotation()));         
+                if(Units.radiansToDegrees(currentRotation) < 0){
+                        currentRotation = Math.PI + (Math.PI + currentRotation);
+                }
+
+                double vW = omegaPID.calculate(currentRotation, targetRotation);
+
+                drive.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+                         0,
+                        vY,
+                        vW,
+                        drive.getPose().getRotation()));         
 	}
 
 	@Override
 	public boolean isFinished() {
 		Pose2d current_pose = drive.getPose();
                 double currentRotation = current_pose.getRotation().getRadians();
-                if(Math.abs(current_pose.getY() - targetY) < 0.05 /*&& Math.abs(targetRotation - currentRotation) < 0.02*/){
+                if(Math.abs(current_pose.getY() - targetY) < 0.05 && Math.abs(targetRotation - currentRotation) < 0.02){
                         return true;
                 }
                 return false;
