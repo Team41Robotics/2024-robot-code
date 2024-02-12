83084420e1ecceeb16caeb0e99a8d2f4994a3b4b
waffleattack <tbaeyens411@gmail.com>
2024-02-12 15:40:23 -0500

    Renamed Max Speed for Clarity

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -10,11 +10,11 @@ public final class Constants {
 	public static final double ROBOT_WIDTH = Units.inchesToMeters(29);
 
 	public static final double SWERVE_MAXSPEED = 4.42;
-	public static final double ANGULAR_SPEED = SWERVE_MAXSPEED / (Math.hypot(ROBOT_LENGTH, ROBOT_WIDTH) / 2);
+	public static final double ANGULAR_MAX_SPEED = SWERVE_MAXSPEED / (Math.hypot(ROBOT_LENGTH, ROBOT_WIDTH) / 2);
 
-	public static final double ANGULAR_SPEED_MULT = 0.3;
+	public static final double ANGULAR_SPEED_MULT = 0.5;
 
-	public static final double SPEED_MULT = 0.3;
+	public static final double SPEED_MULT = 0.55;
 	public static final double SWERVE_WHEEL_RAD = 2 * 2.54 / 100;
 	public static final double L2_DRIVE_RATIO = 1 / 6.75; // input RPM * gearing = output RPM
 	public static final double L2_TURN_RATIO = 7. / 150;
@@ -26,14 +26,14 @@ public final class Constants {
 
 	public static final double APRILTAG_4_Y = 5.548;
 
-	public static final double cam_height = Units.inchesToMeters(9.65);
+	public static final double cam_height = Units.inchesToMeters(13.5);
 
 	public static int SHOOTER_MOTOR_1 = 20; // TBD
 	public static int SHOOTER_MOTOR_2 = 40; // TBD
 	public static int SHOOTER_ANGLE_MOTOR = 60; // TBD
 
 	public static final HolonomicPathFollowerConfig PATH_FOLLOWER_CONFIG = new HolonomicPathFollowerConfig(
-			new PIDConstants(1.0, 0.0, 0.0), // Translation PID constants
+			new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
 			new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
 			4.5, // Max module speed, in m/s
 			0.4488, // Drive base radius in meters. Distance from robot center to furthest module.
